ec77062cbe751cc8fc7ecf1af5994b2b01977931
Sakshyam 3 <94792028+NotSaksh@users.noreply.github.com>
2022-08-30 03:20:39 +0800

    Bug Fix By NotTacos

---
+++ main.py
@@ -0,0 +1,71 @@
+import ctypes
+from os import system
+ctypes.windll.kernel32.SetConsoleTitleW(f"Lua Cloner - Developed by NotSaksh#6969")
+import psutil
+import time
+import sys
+import discord
+import asyncio
+import colorama
+from colorama import Fore, init, Style
+import platform
+from serverclone import Clone
+
+client = discord.Client()
+os = platform.system()
+if os == "Windows":
+    system("cls")
+else:
+    system("clear")
+    print(chr(27) + "[2J")
+print(f"""{Fore.RED}
+
+                                    ██╗░░░░░██╗░░░██╗░█████╗░  ░█████╗░██╗░░░░░░█████╗░███╗░░██╗███████╗██████╗░
+                                    ██║░░░░░██║░░░██║██╔══██╗  ██╔══██╗██║░░░░░██╔══██╗████╗░██║██╔════╝██╔══██╗
+                                    ██║░░░░░██║░░░██║███████║  ██║░░╚═╝██║░░░░░██║░░██║██╔██╗██║█████╗░░██████╔╝
+                                    ██║░░░░░██║░░░██║██╔══██║  ██║░░██╗██║░░░░░██║░░██║██║╚████║██╔══╝░░██╔══██╗
+                                    ███████╗╚██████╔╝██║░░██║  ╚█████╔╝███████╗╚█████╔╝██║░╚███║███████╗██║░░██║
+                                    ╚══════╝░╚═════╝░╚═╝░░╚═╝  ░╚════╝░╚══════╝░╚════╝░╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝
+{Style.RESET_ALL}
+                                                            {Fore.MAGENTA}Developed by: NotSaksh#6969.{Style.RESET_ALL}
+        """)
+token = input(f'Please enter your token:\n >')
+guild_s = input('Please enter guild id you want to copy:\n >')
+guild = input('Please enter guild id where you want to copy:\n >')
+input_guild_id = guild_s  # <-- input guild id
+output_guild_id = guild  # <-- output guild id
+token = token  # <-- your Account token
+
+
+print("  ")
+print("  ")
+
+@client.event
+async def on_ready():
+    extrem_map = {}
+    print(f"Logged In as : {client.user}")
+    print("Cloning Server")
+    guild_from = client.get_guild(int(input_guild_id))
+    guild_to = client.get_guild(int(output_guild_id))
+    await Clone.guild_edit(guild_to, guild_from)
+    await Clone.roles_delete(guild_to)
+    await Clone.channels_delete(guild_to)
+    await Clone.roles_create(guild_to, guild_from)
+    await Clone.categories_create(guild_to, guild_from)
+    await Clone.channels_create(guild_to, guild_from)
+    print(f"""{Fore.GREEN}
+
+
+                                            ░█████╗░██╗░░░░░░█████╗░███╗░░██╗███████╗██████╗░
+                                            ██╔══██╗██║░░░░░██╔══██╗████╗░██║██╔════╝██╔══██╗
+                                            ██║░░╚═╝██║░░░░░██║░░██║██╔██╗██║█████╗░░██║░░██║
+                                            ██║░░██╗██║░░░░░██║░░██║██║╚████║██╔══╝░░██║░░██║
+                                            ╚█████╔╝███████╗╚█████╔╝██║░╚███║███████╗██████╔╝
+                                            ░╚════╝░╚══════╝░╚════╝░╚═╝░░╚══╝╚══════╝╚═════╝░
+
+    {Style.RESET_ALL}""")
+    await asyncio.sleep(5)
+    await client.close()
+
+
+client.run(token, bot=False)
